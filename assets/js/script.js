9042e057fe567a70d7e721c9aea260d22c9d6c33
Ardeya Windyana <ardeya.windy02@gmail.com>
2023-05-04 18:00:04 +0700

    menyatukan js artikel populer dan artikel terbaru

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 
 // API endpoint
 const url = "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=bb2c3dd8bdb84cf9a75cba68741aa1ce";
+let pageNumber = 1;
 
 // Fungsi untuk mengambil data dari API
 const getNews = async () => {
@@ -40,7 +41,7 @@ const renderOtherArticles = async () => {
   let html = "";
 
   otherArticles.forEach((article) => {
-    const { title, urlToImage, url } = article;
+    const { title, urlToImage } = article;
 
     html += `
       <div class="card mb-3">
@@ -64,3 +65,53 @@ const renderOtherArticles = async () => {
 // Memanggil fungsi untuk menampilkan artikel
 renderTopArticle();
 renderOtherArticles();
+
+// artikel terbaru
+// Fungsi untuk menampilkan artikel terbaru
+function renderArticles(articles) {
+  const articlesContainer = document.getElementById("articles-container");
+  articles.forEach((article) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("id", "article-card");
+
+    const image = document.createElement("img");
+    image.src = article.urlToImage;
+    image.alt = article.title;
+    card.appendChild(image);
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const title = document.createElement("h5");
+    title.classList.add("card-title");
+    title.textContent = article.title;
+    cardBody.appendChild(title);
+
+    const description = document.createElement("p");
+    description.classList.add("card-text");
+    description.textContent = article.description;
+    cardBody.appendChild(description);
+
+    card.appendChild(cardBody);
+
+    articlesContainer.appendChild(card);
+  });
+}
+
+
+// Fungsi untuk menampilkan artikel lainnya
+async function loadMoreArticles() {
+  const response = await fetch(url);
+  const data = await response.json();
+  renderArticles(data.articles);
+  pageNumber++;
+}
+
+// Memanggil fungsi artikel lainnya
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadMoreArticles();
+  await loadMoreArticles();
+  const loadMoreButton = document.getElementById("load-more");
+  loadMoreButton.addEventListener("click", loadMoreArticles);
+});
